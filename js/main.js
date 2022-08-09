f6fa0f7c02acec913c0c8ac253896e9c1d96cedd
Vladislav Barabaliuk <103068107+tw1styylol@users.noreply.github.com>
2022-08-09 22:07:41 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider, .slider_items').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+    });
+  });
